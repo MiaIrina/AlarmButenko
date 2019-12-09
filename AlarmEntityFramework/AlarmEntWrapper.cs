cd94c4d57eca74c16c08856d86222aa5de6466ef
Iryna <irynabutenko19@gmail.com>
2019-12-09 08:53:07 +0200

    The interaction with the database is almost implemented

--- AlarmEntityFramework/AlarmEntWrapper.cs
+++ AlarmEntityFramework/AlarmEntWrapper.cs
@@ -1,11 +1,9 @@
-﻿using AlarmProjectButenko.Models;
+﻿
 using Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlarmEntityFramework
 {
